8a8fdd7c04b97e49d989fa4c9a093987cad43fc9
gayatri patne <130586532+gayatripatne@users.noreply.github.com>
2023-08-31 00:49:22 +0530

    Update password generator.py

--- password generator.py
+++ password generator.py
@@ -1,11 +1,11 @@
 import random
 import string
 
-def generating_password(len=10):
+def generating_password(length=10):
     character=string.digit+string.ascii_letter+string.punctuation+string.ascii_letter
     password=''.join(random.choice(characters) for _ in range(len))
     return password
 
 len=int(input("enter the length to password:"))
-password= generate_password(len)
+password= generate_password(length)
 print("generate password",password)
